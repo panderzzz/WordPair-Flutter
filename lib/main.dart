d55b408a58b49288fbfae2fea736e5d836603860
Panderz <sandergarita@gmail.com>
2021-08-19 06:59:07 -0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,4 @@
-// Copyright 2018 The Flutter team. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
+
 
 import 'package:flutter/material.dart';
 import 'package:english_words/english_words.dart';
@@ -64,18 +62,3 @@ class _RandomWordsState extends State<RandomWords> {
   }
 }
 
-
-// *1* The itemBuilder callback is called once per suggested word pairing, 
-//and places each suggestion into a ListTile row. For even rows, the function
-// adds a ListTile row for the word pairing. For odd rows, the function adds 
-//a Divider widget to visually separate the entries. Note that the divider might
-// be difficult to see on smaller devices.
-
-// *2* Add a one-pixel-high divider widget before each row in the ListView.
-
-// *3* The expression i ~/ 2 divides i by 2 and returns an integer result.
-// For example: 1, 2, 3, 4, 5 becomes 0, 1, 1, 2, 2. This calculates the
-// actual number of word pairings in the ListView, minus the divider widgets.
-
-// *4* If you’ve reached the end of the available word pairings, then generate
-// 10 more and add them to the suggestions list.
